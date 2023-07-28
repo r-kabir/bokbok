75d959f353dd4aa0d36f2ec490e90a284f6e792a
RKabir <taposkabir@yahoo.com>
2023-07-28 10:02:00 +0600

    create group validation & notification

--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -1,14 +1,17 @@
-import { Dialog, DialogContent, DialogTitle, TextField, Button, Box, Stack } from '@mui/material'
+import { Dialog, DialogContent, DialogTitle, TextField, Button, Box, Stack, CircularProgress } from '@mui/material'
 import React, {useState} from 'react'
 import { useSelector } from 'react-redux';
 import { getDatabase, ref, onValue, set, remove, push } from "firebase/database";
+import { toast } from 'react-toastify';
 
 let initialGroupInfo = {
     groupname:'',
-    grouptagline:''
+    grouptagline:'',
+    loading: false
 }
 const CreateGroup = () => {
     const db = getDatabase();
+    const notify = (boknotify) => toast.error(boknotify);
     let [dialogModalOpen, setDialogModalOpen] = useState(false)
     let [bokGroupInfo, setBokGroupInfo] = useState(initialGroupInfo);
     let currentuser = useSelector((state)=> state.storeduser.value);
@@ -21,15 +24,29 @@ const CreateGroup = () => {
     }
 
     let handleCreateGroup =()=>{
-        set(push(ref(db, 'bokbokGroupList/')),{
-            groupname: bokGroupInfo.groupname,
-            grouptagline: bokGroupInfo.grouptagline,
-            adminid: currentuser.uid,
-            adminname: currentuser.displayName
-        }).then(() => {
-            setDialogModalOpen(false)
-            
-          });
+        let {groupname, grouptagline} = bokGroupInfo;
+        if(!groupname){notify("no name!!!!")}
+        if(!grouptagline){notify("no tagggggggg!!!!!")}
+        if(groupname && grouptagline){
+            setBokGroupInfo({
+                ...bokGroupInfo,
+                loading:true
+            })
+            set(push(ref(db, 'bokbokGroupList/')),{
+                groupname: bokGroupInfo.groupname,
+                grouptagline: bokGroupInfo.grouptagline,
+                adminid: currentuser.uid,
+                adminname: currentuser.displayName
+            }).then(() => {
+                notify(bokGroupInfo.groupname+" group is successfully created by "+currentuser.displayName)
+                setBokGroupInfo({
+                    groupname:"",
+                    grouptagline:"",
+                    loading: false
+                })
+                setDialogModalOpen(false)
+              })
+        }
     }
 
   return (
@@ -40,9 +57,14 @@ const CreateGroup = () => {
         <DialogContent>
           <Stack gap={3}>
             <h2>Set Group Name & Tag Line to Create Your Own Group</h2>
-            <TextField onChange={handleChange} name='groupname' fullWidth variant='outlined' color='warning' label="Group Name" />
-            <TextField onChange={handleChange} name='grouptagline' fullWidth variant='outlined' color='warning' label="Group Tag Line"/>
-            <Button onClick={handleCreateGroup} variant="contained" color='warning' sx={{width:"50%"}}>Create</Button>
+            <TextField onChange={handleChange} value={bokGroupInfo.groupname} name='groupname' fullWidth variant='outlined' color='warning' label="Group Name" />
+            <TextField onChange={handleChange} value={bokGroupInfo.grouptagline} name='grouptagline' fullWidth variant='outlined' color='warning' label="Group Tag Line"/>
+            {
+                bokGroupInfo.loading ?
+                <CircularProgress color='warning'/>
+                :
+                <Button onClick={handleCreateGroup} variant="contained" color='warning' sx={{width:"50%"}}>Create</Button>
+            }
           </Stack>
         </DialogContent>
       </Dialog>
