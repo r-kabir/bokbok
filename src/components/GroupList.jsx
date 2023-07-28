207084050600a03bd35ba1e5e2ae51584768546c
RKabir <taposkabir@yahoo.com>
2023-07-28 15:07:32 +0600

    group list & my group list setup completed

--- src/components/GroupList.jsx
+++ src/components/GroupList.jsx
@@ -1,21 +1,40 @@
 import { Button, Box } from '@mui/material'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import tapos from '../assets/tapos.png';
 import CreateGroup from './CreateGroup';
+import { getDatabase, ref, onValue, set, remove, push } from "firebase/database";
+import { useSelector } from 'react-redux';
 
 const GroupList = () => {
+  const db = getDatabase();
+  let currentuser = useSelector((state)=> state.storeduser.value);
+  let [bokAllGroup, setBokAllGroup] = useState([]);
 
+  useEffect(()=>{
+    const hudaiRef = ref(db, 'bokbokGroupList/')
+    onValue(hudaiRef, (snapshot)=>{
+      let hudaiArray = [];
+      snapshot.forEach(bokitem=>{
+        if(currentuser.uid != bokitem.val().adminid){
+          hudaiArray.push({...bokitem.val(), id: bokitem.key})
+        }
+      })
+      setBokAllGroup(hudaiArray);
+    })
+  },[])
   return (
     <Box boxShadow={4} sx={{height:"40vh", overflow:"auto", borderRadius:"10px"}}>
         <Box sx={{p:"15px", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
           <h3>Group List</h3>
           <CreateGroup />
         </Box>
-        <Box sx={{display:"flex", alignItems:"center", justifyContent:"space-between",borderBottom:"1px solid red", p:"10px"}}>
-            <img className="tapos" src={tapos} />
-            <Box><h4>Tapos Kabir</h4><p>ghjgfjfhkgk hkhggkgkk</p></Box>
-            <Button size='small' variant='contained' color='error' sx={{textTransform:'capitalize', fontSize:'11px'}}>Join Group</Button>
-        </Box>
+        {bokAllGroup.map(bokitem=>(
+          <Box sx={{display:"flex", alignItems:"center", justifyContent:"space-between",borderBottom:"1px solid red", p:"10px"}}>
+          <img className="tapos" src={tapos} />
+          <Box><h4>{bokitem.groupname}</h4><p>{bokitem.grouptagline}</p></Box>
+          <Button size='small' variant='contained' color='error' sx={{textTransform:'capitalize', fontSize:'11px'}}>Join Group</Button>
+          </Box>
+        ))}
     </Box>
   )
 }
--- src/components/MyGroups.jsx
+++ src/components/MyGroups.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Box, Button } from '@mui/material'
 import tapos from '../assets/tapos.png';
-
+import CreateGroup from './CreateGroup';
+import { getDatabase, ref, onValue, set, remove, push } from "firebase/database";
+import { useSelector } from 'react-redux';
 
 const MyGroups = () => {
+  const db = getDatabase();
+  let currentuser = useSelector((state)=> state.storeduser.value);
+  let [bokMyGroup, setBokMyGroup] = useState([]);
+
+  useEffect(()=>{
+    const hudaiRef = ref(db, 'bokbokGroupList/')
+    onValue(hudaiRef, (snapshot)=>{
+      let hudaiArray = [];
+      snapshot.forEach(bokitem=>{
+        if(currentuser.uid == bokitem.val().adminid){
+          hudaiArray.push({...bokitem.val(), id: bokitem.key})
+        }
+      })
+      setBokMyGroup(hudaiArray);
+    })
+  },[])
   return (
     <Box boxShadow={4} sx={{height:"40vh", overflow:"auto", borderRadius:"10px"}}>
         <Box sx={{p:"15px", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
           <h3>My Groups</h3>
-          <Button variant='outlined' size='small' color='warning'>Create Group</Button>
+          <CreateGroup />
         </Box>
-        <Box sx={{display:"flex", alignItems:"center", justifyContent:"space-between",borderBottom:"1px solid red", p:"10px"}}>
-            <img className="tapos" src={tapos} />
-            <Box><h4>Tapos Kabir</h4><p>ghjgfjfhkgk hkhggkgkk</p></Box>
-            <Button size='small' variant='contained' color='error' sx={{textTransform:'capitalize', fontSize:'11px'}}>Leave Group</Button>
-        </Box>
+        {bokMyGroup.map(bokitem=>(
+          <Box sx={{display:"flex", alignItems:"center", justifyContent:"space-between",borderBottom:"1px solid red", p:"10px"}}>
+          <img className="tapos" src={tapos} />
+          <Box><h4>{bokitem.groupname}</h4><p>{bokitem.grouptagline}</p></Box>
+          <Button size='small' variant='contained' color='error' sx={{textTransform:'capitalize', fontSize:'11px'}}>Leave Group</Button>
+          </Box>
+        ))}
     </Box>
   )
 }
